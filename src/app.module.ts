89d00f71f3f9fe4c3814a594a8d35c7563f94b85
RAHUL R B <rahulbalakrishnan6169@gmail.com>
2023-01-23 12:36:36 +0530

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AuthController } from './auth';
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController,AuthController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 
+
 @Controller('auth')
+
 export class AuthController {
   @Post('signup')
   async signup(@Body() form) {
     
     console.log(form)
+    
   }
 }
