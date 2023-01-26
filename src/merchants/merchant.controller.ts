64132014fe0c0b34123983ca7b6f077a2e184774
RAHUL R B <rahulbalakrishnan6169@gmail.com>
2023-01-26 12:49:04 +0530

    updated

--- src/merchants/merchant.controller.ts
+++ src/merchants/merchant.controller.ts
@@ -1,30 +1,28 @@
-import { Controller, Post, Body,Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Put, Param } from '@nestjs/common';
 import { merchantsData } from './merchant.constant';
 import { MerchantService } from './merchant.service';
 
 @Controller()
-
 export class MerchantController {
   constructor(private readonly merchantService: MerchantService) {}
   @Post('createMerchant')
   async createMerchant(@Body() merchantData) {
-    const newMerchantData= this.merchantService.createMerchant(merchantData)
-   return newMerchantData;
-  } 
-    
-  @Get('getAllMerchants')
-  getAllMerchants(){
-    
-    return merchantsData
-    
+    console.log(merchantData)
+    const newMerchantData = this.merchantService.createMerchant(merchantData?.formValues);
+    return newMerchantData;
   }
-  // @Put('editMerchant')
-  // editMerchant(){
 
-  // }
+  @Get('getAllMerchants')
+  getAllMerchants() {
+    return  this.merchantService.getAllMerchants();
+  }
 
+  @Put('getMerchant/:id?')
+
+updateMerchant( @Body() body) {
+   console.log(body)
   
+  
+    return this.merchantService.updateMerchant(body?.id);
 }
-
-
-
+}
--- src/merchants/merchant.service.ts
+++ src/merchants/merchant.service.ts
@@ -2,15 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { Merchant, merchantsData } from './merchant.constant';
 @Injectable()
 export class MerchantService {
-
-  createMerchant(merchantData:Merchant):Merchant {
-const idUpdatedMerchantData={...merchantData,id:Math.random()}
-      merchantsData?.push(idUpdatedMerchantData);
-    // console.log(merchantsData);
+  createMerchant(merchantData: Merchant): Merchant {
+    const idUpdatedMerchantData = { ...merchantData, id: Math.random() * 5 };
+    merchantsData?.push(idUpdatedMerchantData);
+    console.log(merchantsData);
     return idUpdatedMerchantData;
   }
-  getAllMerchants():Merchant[]{
+
+  getAllMerchants(): Merchant[] {
     return merchantsData;
   }
 
+  updateMerchant(id: number) {
+  //   for (let i = 0; i < merchantsData.length; i++) {
+  //     if (merchantsData[i].id === id) {
+  //       merchantsData[i] = { ...merchantsData[i], ...updatedMerchantData };
+
+  //       return merchantsData[i];
+  //     }
+  //   }
+  //   return null;
+  // }
+  const editMerchantData=merchantsData.find((merchant)=>merchant?.id===id)
+  return editMerchantData
+}
 }
