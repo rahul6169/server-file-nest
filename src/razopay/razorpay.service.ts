dc9e437ee2b4d3c08ae0ceaffdfffc79c4425ea2
rahul6169 <rahulbalakrishnan6169@gmail.com>
2023-08-09 10:33:56 +0530

    small changes

--- src/razopay/razorpay.service.ts
+++ src/razopay/razorpay.service.ts
@@ -6,8 +6,6 @@ import Razorpay from 'razorpay';
 @Injectable()
 export class RazorPayService {
   constructor(private readonly prisma: PrismaService) {}
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-
   async createPaymentOrder(
     amount: number,
     currency = 'INR',
@@ -16,18 +14,16 @@ export class RazorPayService {
       key_id: 'rzp_test_4xcvSagUAjt2Wl',
       key_secret: 'uUEZBl3mMDAklV5qn11Z471H',
     });
-    // const bfCode = await this.bfCounterService.getBfCode(BFCounterType.ORDER)
     const order = await instance.orders.create({
       amount: Number((amount * 100).toFixed(0)),
       currency,
-      // receipt: bfCode,
     });
     console.log(order);
     if (!order) throw new Error();
     return (await this.prisma.razorpay.create({
       data: {
         amount: order?.amount as any,
-        orderId:order?.id
+        orderId: order?.id,
       },
     })) as unknown as any;
   }
